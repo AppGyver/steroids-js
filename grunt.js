3fb5358aa65ecad6ac763ef324602a17967773b2
Jesse Ikonen <jesse.ikonen@gmail.com>
2012-12-14 15:52:07 -0800

    Generate docs

--- grunt.js
+++ grunt.js
@@ -21,28 +21,53 @@ module.exports = function(grunt) {
 
     watch: {
         files: ['src/**/*'],
-        tasks: 'coffee concat'
+        tasks: 'coffee concat:dist shell:generate_documentation concat:docs'
     },
 
     concat: {
       dist: {
         src: ['<banner>',
-              '<file_strip_banner:lib/SteroidsNativeObject.js>',
+              '<file_strip_banner:lib/bridges/native/NativeBridge.js>',
+              '<file_strip_banner:lib/NativeObject.js>',
+              '<file_strip_banner:lib/models/Device.js>',
+              '<file_strip_banner:lib/models/App.js>',
+              '<file_strip_banner:lib/models/Button.js>',
               '<file_strip_banner:lib/models/Layer.js>',
               '<file_strip_banner:lib/models/Tab.js>',
               '<file_strip_banner:lib/models/NavigationBar.js>',
               '<file_strip_banner:lib/Steroids.js>'],
         dest: 'dist/steroids.js',
         separator: ';'
+      },
+      docs: {
+        src: ['docs/Steroids.html',
+              'docs/App.html',
+              'docs/Device.html',
+              'docs/Button.html',
+              'docs/NavigationBar.html',
+              'docs/Layer.html',
+              'docs/Tab.html',
+              'docs/NativeObject.html',
+              'docs//NativeBridge.html'],
+        dest: 'docs/<%= pkg.name %>-<%= pkg.version %>.html',
+        separator: '<hr>'
       }
+    },
+
+    shell: {
+        generate_documentation: {
+            command: 'npm run-script gdocs'
+        }
     }
   });
 
   // Load tasks from "grunt-sample" grunt plugin installed via Npm.
   grunt.loadTasks('grunt-sample');
+  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-coffee');
 
   // Default task.
   grunt.registerTask('default', 'coffee');
+
 
 };
