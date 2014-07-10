86c9ad5a105bafb51ff51e885496987aaa2e4691
Merituuli Melkko <merituuli.melkko@appgyver.com>
2014-07-10 14:48:50 +0300

    testApp deploy fixed

--- testApp/deploy-all.rb
+++ testApp/deploy-all.rb
@@ -6,6 +6,10 @@
   system("cp config/#{folder}/cloud.json.#{ARGV[0]} config/cloud.json")
   system("cp config/#{folder}/rotations.js www/javascripts")
 
-  system("steroids deploy")
-
+  if ARGV[0] == "test"
+  	system("steroids deploy --ankaURL=http://anka.testgyver.com --shareURL=http://share.testgyver.com --authURL=http://accounts.testgyver.com")
+  else
+  	system("steroids deploy")
+  end
+  
 end
